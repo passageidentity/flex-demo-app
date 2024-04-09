302e337cdb47f98f682a6a219d873ee37b51fa71
Kevin Flanagan <kevin.flanagan@passage.id>
2024-04-09 10:52:29 -0500

    basic monorepo + express backend

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,24 @@
+// src/index.ts
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { usersRouter } from "./routes/users";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server + Watching for changes');
+});
+
+app.use('/users', usersRouter);
+
+async function main(){
+  console.log('starting server...')
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}
+
+main();
